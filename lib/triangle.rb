2645f997654b0b48fa4ab8698ffc305bc23d44e0
Evans Rutto <robbyevans001@gmail.com>
2022-08-25 10:58:37 +0300

    commit tree d6874119a24958b4f32a74a6bb4eb8674b6904d7

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
   # write code here
+  attr_reader :s1, :s2, :s3
+
+  def initialize(s1,s2,s3)
+    @s1=s1
+    @s2=s2
+    @s3=s3
+  end  
+  
+  def kind
+    validate_triangle
+      if  s1 == s2 && s2 == s3
+        :equilateral
+      elsif s1 == s2 || s2  == s3 || s3 == s1 
+        :isosceles
+      else  
+        :scalene
+      end
+  end    
+
+
+  def validate_triangle
+    real_triangle =  [(s1 + s2 > s3), (s1 + s3 > s2), (s2 + s3 > s1)]
+      [s1, s2, s3].each do |side|
+        real_triangle << false if side <= 0 
+      raise TriangleError if real_triangle.include?(false)
+      end
+  end
+      
+  class TriangleError < StandardError
+    def message
+      "Triangle inequality failed"
+    end
+  end
+
 end
